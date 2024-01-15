96a7865273b1b48e3c8bf1cebe4a1f73bb85f496
Rattan Ashat <rashat@lakeheadu.ca>
2024-01-15 15:53:52 -0500

    Added getValue() method to card class and testing methods in main class

--- src/main/java/com/example/comp1008w2/Main.java
+++ src/main/java/com/example/comp1008w2/Main.java
@@ -2,11 +2,13 @@ package com.example.comp1008w2;
 
 public class Main {
     public static void main(String[] args) {
-        Card card1 = new Card("heart","ace");
-
+        Card card1 = new Card("Heart", "king");
 
         System.out.println(card1);
-        System.out.println(card1.getColour());
-        System.out.println(card1.getFaceName());
+        System.out.println(card1 + " Colour: " + card1.getColour());
+        System.out.println(card1 + " Value: " + card1.getValue());
+
+        Card card2 = new Card("Spade", "Ace");
+        System.out.println(card2);
     }
 }
