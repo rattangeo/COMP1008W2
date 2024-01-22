2cc29e02556eafbc626b1a2308f1af512bc23127
Rattan Ashat <rashat@lakeheadu.ca>
2024-01-22 15:03:20 -0500

    completed DeckOfCards class, added shuffle() & dealTopCard()

--- src/main/java/com/example/comp1008w2/DeckOfCards.java
+++ src/main/java/com/example/comp1008w2/DeckOfCards.java
@@ -1,6 +1,7 @@
 package com.example.comp1008w2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DeckOfCards {
@@ -14,7 +15,26 @@ private ArrayList<Card> deck;
     public DeckOfCards() {
         deck = new ArrayList<>();
 
-        List<String> suits = Card.
+        List<String> suits = Card.getValidSuits();
+        List<String> faceNames = Card.getFaceNames();
+//Looping against card objects -faceNames & suits to build a deck
+        for (int i=0; i <suits.size();i++){
+            for (int x=0; x <faceNames.size(); x++){
+                //Building the deck, adding Cards to deck one at a time
+                deck.add(new Card(suits.get(i),faceNames.get(x)));
+            }
+        }
 
     }
+
+    public  Card dealTopCard(){
+        if (deck.size()>0)
+            return deck.remove(0);
+        return null;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(deck);
+    }
+
 }
--- src/main/java/com/example/comp1008w2/Main.java
+++ src/main/java/com/example/comp1008w2/Main.java
@@ -1,14 +1,32 @@
 package com.example.comp1008w2;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Card card1 = new Card("Heart", "king");
+//        Card card1 = new Card("Heart", "king");
+//
+//        System.out.println(card1);
+//        System.out.println(card1 + " Colour: " + card1.getColour());
+//        System.out.println(card1 + " Value: " + card1.getValue());
+//
+//        Card card2 = new Card("Spade", "Ace");
+//        System.out.println(card2);
 
-        System.out.println(card1);
-        System.out.println(card1 + " Colour: " + card1.getColour());
-        System.out.println(card1 + " Value: " + card1.getValue());
+        DeckOfCards deck = new DeckOfCards();
 
-        Card card2 = new Card("Spade", "Ace");
-        System.out.println(card2);
+        //deck.dealTopCard();
+        ArrayList<Card> player1Hand = new ArrayList<>();
+        ArrayList<Card> player2Hand = new ArrayList<>();
+        deck.shuffle();
+        for (int i=1; i<=5; i++){
+            player1Hand.add(deck.dealTopCard());
+            player2Hand.add(deck.dealTopCard());
+        }
+
+        System.out.println("Player 1: " + player1Hand);
+        System.out.println("Player 2: " + player2Hand);
+
+
     }
 }
